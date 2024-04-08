15274478123e7cb62c0f845715b759207f9539fb
DQTRUNGDQ <trungthpthy@gmail.com>
2024-04-08 22:19:31 +0700

    updated-2 08/04/2024

--- resources/views/frontend/blog.blade.php
+++ resources/views/frontend/blog.blade.php
@@ -70,6 +70,60 @@
                 </ul>
             </div>
         </div>
+
+        <div class="content-container">
+            <div class="content-sidebar-wrap">
+                <div class="content">
+                    <article class="entry">
+                        <header class="entry-header">
+                            <h2 class="entry-title">
+                                <a href="">
+                                    Bài Viết Blog đầu tay: Giới thiệu một chút về Blog BookWorld
+                                </a>
+                            </h2>
+                            <div class="entry-meta">
+                                <span class="entry-time">April 08, 2024</span>
+                                <span class="entry-author">
+                                    <a href="">
+                                        <span class="entry-author-name">Quốc Trung</span>
+                                    </a>
+                                </span>
+                                <span class="entry-comment-links">
+                                    <a href="">14 Comments</a>
+                                </span>
+                            </div>
+                        </header>
+
+                        <div class="entry-content">
+                            <a href="" class="entry-image-links">
+                                <img width="600" height="400" src="../../../public/upload/img/blog/greece.avif"
+                                    alt="">
+                            </a>
+                            <p>
+                                Xin chào các bạn, đây trang web blog cá nhân được mở rộng ra từ BookWorld,
+                                ở đây mình sẽ chia sẻ trải nghiệm trong cuộc sống, nghiên cứu, và làm việc của tôi
+                                (Trung), review về những gì liên quan đến sách...Và hy vọng qua blog của tôi, bạn tìm
+                                thấy điều gì đó có ích cho cuộc sống của bạn.
+
+                                <a href="" class="more-links">[Read more...]</a>
+                            </p>
+
+                        </div>
+
+                        <footer class="entry-footer">
+                            <p class="entry-meta">
+                                <span class="entry-categories">
+                                    Filed Under:
+                                    <a href="">Cuộc sống (LIFE)</a> ,
+                                    <a href="">Chia Sẻ</a>
+                                </span>
+                            </p>
+                        </footer>
+                    </article>
+                </div>
+                <div class="sidebar"></div>
+            </div>
+        </div>
     </div>
 
 </body>
